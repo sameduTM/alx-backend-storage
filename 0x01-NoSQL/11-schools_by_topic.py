8791e395e74b5d5aa9a7d25e5362503d02dd6588
Ken Wekesa <wanyamak884@gmail.com>
2025-06-01 20:12:16 +0300

    returrn list of school having a specific topic

---
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""11. Where can I learn Python?
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """Python function that returns the list of school having a specific topic
+    """
+    return mongo_collection.find({'topics': 'Python'})
+   
+        
